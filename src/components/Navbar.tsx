a47fda31d51bb5ff1cfd79be7762d48c8b11d2aa
Mahrad <mahradti@gmail.com>
2024-09-29 20:09:58 +0330

    ProductCount added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FaCartShopping } from "react-icons/fa6";
 import { IoCloseSharp } from "react-icons/io5";
+import { CartContext } from "../context/CartContext";
 
 const Navbar = () => {
 
@@ -13,12 +14,16 @@ const Navbar = () => {
         setShowModal(false)
     }
 
+    const cart = useContext(CartContext)
+
+    const productCount = cart.items.reduce((sub , product)=> sub + product.quantity , 0)
+
     return (
         <>
             <div className="flex items-center cursor-pointer justify-end border-b border-gray-300 py-5 ">
                 <div className="flex items-center gap-2 border border-white px-3 py-2 rounded-lg" onClick={openModal}>
                     <FaCartShopping className="text-green-500 text-xl" />
-                    <p className="font-bold text-xl text-white">سبد خرید </p>
+                    <p className="font-bold text-xl text-white"> ({productCount}) سبد خرید </p>
                 </div>
             </div>
             {showModal && (
